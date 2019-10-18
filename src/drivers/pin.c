61a9b75ab9a03dd22c6f468b90e8d02cbfdd18e6
Clayton Brutus <cb285@evansville.edu>
2019-10-17 20:20:37 -0500

    added interrupt registering/handling (not working)

--- src/drivers/pin.c
+++ src/drivers/pin.c
@@ -1,5 +1,8 @@
 #include "pin.h"
 #include "drivers/clock.h"
+#include "drivers/irq.h"
+
+#include "utils/debug.h"
 
 // mode register values
 #define _MODER_INPUT  0x0ul
@@ -26,7 +29,202 @@
 #define _PUPDR_DOWN 0x2ul
 #define _PUPDR_MASK 0x3ul
 
+#define _MAX_IRQS 16
+#define _PRIO_DISABLED 0xfffffffful
+
+#define _MIN(x, y) ((x < y)? x : y)
+
 #define _PORT(x) ((GPIO_TypeDef*)x)
+
+typedef struct {
+    bool used;
+    bool enabled;
+    pin_port_t port;
+    pin_num_t pin;
+    uint32_t irq_priority;         // interrupt priority
+    pin_irq_handler_t irq_handler; // interrupt handler function
+} _irq_slot_t;
+
+static uint32_t _exti04_priorities[5] = { _PRIO_DISABLED, _PRIO_DISABLED, _PRIO_DISABLED, _PRIO_DISABLED, _PRIO_DISABLED };
+static uint32_t _exti59_priority = _PRIO_DISABLED;
+static uint32_t _exti1015_priority = _PRIO_DISABLED;
+
+static bool _exti_used[16] = {};
+static _irq_slot_t _irq_slots[_MAX_IRQS] = {};
+
+void _HandleIrq(uint8_t exti) {
+    // find and call handler
+    for(uint8_t i = 0; i < _MAX_IRQS; i++) {
+	if(_irq_slots[i].used && _irq_slots[i].enabled && _irq_slots[i].pin == exti)
+	    _irq_slots[i].irq_handler();
+    }
+    
+    // clear pending bit
+    EXTI->PR = (1ul << exti);
+}
+
+void EXTI0_IRQHandler(void) {
+    _HandleIrq(0);
+}
+
+void EXTI1_IRQHandler(void) {
+    _HandleIrq(1);
+}
+
+void EXTI2_IRQHandler(void) {
+    _HandleIrq(2);
+}
+
+void EXTI3_IRQHandler(void) {
+    _HandleIrq(3);
+}
+
+void EXTI4_IRQHandler(void) {
+    _HandleIrq(4);
+}
+
+void EXTI9_5_IRQHandler(void) {
+    for(uint8_t bit = 5; bit < 10; bit++)
+	if(EXTI->PR & (1ul << bit))
+	    _HandleIrq(bit);
+}
+
+void EXTI15_10_IRQHandler(void) {
+
+    Debug_Print("EXTI IRQ");
+    
+    for(uint8_t bit = 10; bit < 16; bit++)
+	if(EXTI->PR & (1ul << bit))
+	    _HandleIrq(bit);
+}
+
+// find a currently allocated slot
+static _irq_slot_t* _FindIrqSlot(const pin_port_t port, const pin_num_t pin) {
+    for(uint8_t i = 0; i < _MAX_IRQS; i++) {
+	if(_irq_slots[i].used) {
+	    if((_irq_slots[i].port == port) && (_irq_slots[i].pin == pin))
+		return &_irq_slots[i];
+	}
+    }
+    return NULL;
+}
+
+// find available irq slot
+static _irq_slot_t* _AllocateIrqSlot(const pin_port_t port, const pin_num_t pin) {
+
+    // check if EXTI is already being used
+    if(_exti_used[pin])
+	return NULL;
+
+    // prevent re-registering same pin
+    if(_FindIrqSlot(port, pin))
+	return NULL;
+    
+    // find empty slot
+    for(uint8_t i = 0; i < _MAX_IRQS; i++) {
+	if(!_irq_slots[i].used) {
+	    _irq_slots[i].used = true;
+	    _exti_used[pin] = true;
+	    return &_irq_slots[i];
+	}
+    }
+    return NULL;
+}
+/*
+static void _FreeIrqSlot(const pin_port_t port, const pin_num_t pin) {
+
+    // check if actually allocated
+    if(!_exti_used[pin])
+	return;
+    
+    // find and free slot
+    for(uint8_t i = 0; i < _MAX_IRQS; i++) {
+	if((_irq_slots[i].port == port) && (_irq_slots[i].pin == pin)) {
+	    _irq_slots[i].used = false;
+	    _exti_used[pin] = false;
+	    return;
+	}
+    }
+}
+*/
+// get highest priority (lowest number) of currently registered interrupts
+static uint32_t _GetIrqPriority(pin_num_t pin, uint32_t priority) {
+    if(pin <= 4)
+	return _MIN(priority, _exti04_priorities[pin]);
+    else if(pin <= 9)
+	return _MIN(priority, _exti59_priority);
+    else
+	return _MIN(priority, _exti1015_priority);
+}
+
+static bool _RegisterIrq(const pin_options_t* options) {
+    
+    // check if should setup interrupt
+    if(options->irq_mode == PIN__IRQ_MODE__NONE)
+	return true;
+    
+    // check if irq handler is given
+    if(!options->irq_handler)
+	return false;
+    
+    // allocate irq slot
+    _irq_slot_t* slot = _AllocateIrqSlot(options->port, options->pin);
+    
+    // check if got slot
+    if(!slot)
+	return false;
+
+    // fill slot info
+    slot->port = options->port;
+    slot->pin = options->pin;
+    slot->irq_priority = options->irq_priority;
+    slot->irq_handler = options->irq_handler;
+    
+    // select correct EXTICR
+    uint8_t exticr = options->pin / 4;
+    
+    // get position of EXTIx field inside EXTICR
+    uint8_t exticr_pos = (options->pin % 4) * 4;
+    
+    // check if already being used?
+    //if(SYSCFG->EXTICR[exticr] & (0xf << exticr_pos))
+    //return false;
+    
+    // set EXTIx field in EXTICR (port A = 0, port B = 1, ...)
+    uint8_t exticr_extix = ((uint32_t)options->port - (uint32_t)GPIOA) / ((uint32_t)GPIOB - (uint32_t)GPIOA);
+    
+    // set EXTICR
+    SYSCFG->EXTICR[exticr] |= ((uint32_t)exticr_extix << exticr_pos);
+    
+    // unmask interrupt
+    EXTI->IMR |= (1ul << options->pin);
+    
+    // enable rising/falling interrupt
+    if(options->irq_mode == PIN__IRQ_MODE__RISING)
+	EXTI->RTSR |= (1ul << options->pin);
+    else
+	EXTI->FTSR |= (1ul << options->pin);
+    
+    // select IRQn
+    uint32_t irq;
+    if(options->pin <= 4)
+	irq = EXTI0_IRQn + options->pin;
+    else if(options->pin <= 9)
+	irq = EXTI9_5_IRQn;
+    else
+	irq = EXTI15_10_IRQn;
+    
+    // set IRQn priority
+    Irq_SetPriority(irq, _GetIrqPriority(options->pin, options->irq_priority));
+    
+    // enable
+    slot->enabled = true;
+    Irq_Enable(irq);
+    
+    Clock_EnablePeripheral(SYSCFG);
+    
+    return true;
+}
 
 bool Pin_Create(const pin_options_t* options) {
     
@@ -104,9 +302,9 @@ bool Pin_Create(const pin_options_t* options) {
     
     // pull up/down (default none)
     port->PUPDR &= ~(_PUPDR_MASK << (options->pin * 2));
-    if(options->attr * PIN__ATTR__PULL_UP)
+    if(options->attr & PIN__ATTR__PULL_UP)
 	port->PUPDR |= (_PUPDR_UP << (options->pin * 2));
-    if(options->attr * PIN__ATTR__PULL_DOWN)
+    if(options->attr & PIN__ATTR__PULL_DOWN)
 	port->PUPDR |= (_PUPDR_DOWN << (options->pin * 2));
     
     // set speed (default low)
@@ -117,8 +315,13 @@ bool Pin_Create(const pin_options_t* options) {
 	port->OSPEEDR |= (_OSPEEDR_HIGH << (options->pin * 2));
     else if(options->attr & PIN__ATTR__SPEED_HIGHEST)
 	port->OSPEEDR |= (_OSPEEDR_HIGHEST << (options->pin * 2));
+    
+    // setup interrupt (if enabled)
+    return _RegisterIrq(options);
+}
 
-    return true;
+void Pin_EnableIrq(const pin_port_t port, const pin_num_t pin) {
+    
 }
 
 void Pin_Set(const pin_port_t port, const pin_num_t pin) {
--- src/drivers/pin.h
+++ src/drivers/pin.h
@@ -1,8 +1,8 @@
 #ifndef _PIN_H
 #define _PIN_H
 
-#include <stdbool.h>
 #include "stm32f4xx.h"
+#include <stdbool.h>
 
 typedef enum {
     PIN__MODE__INPUT,
@@ -27,6 +27,14 @@ typedef enum {
     PIN__MODE__AF15
 } pin_mode_t;
 
+typedef enum {
+    PIN__IRQ_MODE__NONE, // default
+    PIN__IRQ_MODE__RISING,
+    PIN__IRQ_MODE__FALLING
+    //PIN__IRQ_MODE__HIGH, // not possible?
+    //PIN__IRQ_MODE__LOW
+} pin_irq_mode_t;
+
 #define PIN__ATTR__OPEN_DRAIN     (0x1 << 0)
 #define PIN__ATTR__PUSH_PULL      (0x0) // default
 #define PIN__ATTR__PULL_UP        (0x1 << 1)
@@ -40,12 +48,16 @@ typedef enum {
 typedef uint8_t pin_attr_t;
 typedef void*   pin_port_t;
 typedef uint8_t pin_num_t;
+typedef void (*pin_irq_handler_t)(void);
 
 typedef struct {
-    pin_port_t port;     // port
-    pin_num_t pin;       // number in port
-    pin_mode_t mode;     // mode
-    pin_attr_t attr;     // attributes
+    pin_port_t port;               // port
+    pin_num_t pin;                 // number in port
+    pin_mode_t mode;               // mode
+    pin_attr_t attr;               // attributes
+    pin_irq_mode_t irq_mode;       // interrupt mode
+    uint32_t irq_priority;         // interrupt priority
+    pin_irq_handler_t irq_handler; // interrupt handler function
 } pin_options_t;
 
 bool Pin_Create(const pin_options_t* options);
